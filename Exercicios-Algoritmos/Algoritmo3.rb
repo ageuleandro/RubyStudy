584f7e034acb103c15fb2bc388b837241d35d237
Ageu Leandro <leandro.junior7@gmail.com>
2025-01-23 13:23:30 -0300

    Adicionado comentario no Algoritmo3

--- Exercicios-Algoritmos/Algoritmo3.rb
+++ Exercicios-Algoritmos/Algoritmo3.rb
@@ -1,3 +1,8 @@
+# Desafio: Número Palíndromo
+# Escreva um programa em Ruby que:
+# Verifique se o número é um palíndromo (ou seja, ele é igual quando lido de trás para frente).
+# Informe ao usuário se o número é ou não um palíndromo.
+
 
 pali = 121
 
