62628a4fc94208e5fd02b4dc9158c31f828115d9
Ageu Leandro <leandro.junior7@gmail.com>
2025-01-24 11:26:09 -0300

    Algoritmo07

--- Exercicios-Algoritmos/Algoritmo07.rb
+++ Exercicios-Algoritmos/Algoritmo07.rb
@@ -1,3 +1,22 @@
-# Problema: Conversão de Números Romanos para Inteiros
-# Você precisa implementar uma função que converta um número romano em seu equivalente numérico (inteiro).
-#
+# Problema: Conversão de Números inteiros para Romanos
+# Você precisa implementar uma função que converta um número inteiro em seu equivalente Romano.
+
+def mudar_para_romano(numero)
+  romanos_mapeados = {
+    1000 => "M", 900 => "CM", 500 => "D", 400 => "CD",
+    100 => "C", 90 => "XC", 50 => "L", 40 => "XL",
+    10 => "X", 9 => "IX", 5 => "V", 4 => "IV", 1 => "I"
+  }
+  resultado = ""
+
+  romanos_mapeados.each do |valor, romano|
+    while numero >= valor
+      resultado += romano
+      numero -= valor
+    end
+  end
+
+  resultado
+end
+
+puts mudar_para_romano(33)
